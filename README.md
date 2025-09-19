131de197202d94713b62bacfcfe330e9d0ff8338
Jason Youzwak <jyouzwak@peratonlabs.com>
2025-09-19 08:50:08 -0400

    Add user setup

--- README.md
+++ README.md
@@ -10,3 +10,10 @@ Requires sudo:
 
 - `ubuntu-tools.sh`: Install standard utilties (e.g., vim, tmux, etc.)
 - `ubuntu-dev.sh`: Install development tools
+
+## General Configuration
+
+Hide the side bar
+
+- Settings -> Appearance -> Auto-hide the Dock
+
--- bin/ubuntu-tools.sh
+++ bin/ubuntu-tools.sh
@@ -2,9 +2,14 @@
 
 # Apt install utilities
 sudo apt install \
+    gedit-plugin-text-size \
     git \
+    gnome-tweaks \
     meld \
     ncdu \
     ripgrep \
     tmux \
     vim
+
+# Gnome Tweaks: Remap CapsLock -> Control
+# Gedit Plugin Text Size: Allow keyboard shortcut to resize text in Gedit
